cebcd3bfd35727a9e705bdd4631f1506636f9bec
joshua03456 <80635235+joshua03456@users.noreply.github.com>
2021-07-07 19:38:51 +0530

    Add files via upload

---
+++ python program to find the greatest number.py
@@ -0,0 +1,10 @@
+a=int(input("enter the 1st number:"))
+b=int(input("enter the 2nd number:"))
+c=int(input("enter the 3rd number:"))
+if a>b and a>c:
+    print(" 1st is great")
+elif b>a and b>c:
+    print("2nd is great")
+else:
+    print("3rd is great")
+    
