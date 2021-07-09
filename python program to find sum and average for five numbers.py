ffa2fd2634e1c9e0275cb022ba6008bde181335e
joshua03456 <80635235+joshua03456@users.noreply.github.com>
2021-07-09 14:23:46 +0530

    Add files via upload

---
+++ python program to find sum and average for five numbers.py
@@ -0,0 +1,12 @@
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+c=int(input("enter the number:"))
+d=int(input("enter the number:"))
+e=int(input("enter the number:"))
+
+
+total=a+b+c+d+e
+print("TOTAL:",total)
+
+average=total//5
+print("AVERAGE:",average)
