124291d76b413ced3641061dab417c93714b1d50
joshua03456 <80635235+joshua03456@users.noreply.github.com>
2022-01-19 16:49:55 +0530

    Update python program to find the greatest number.py

--- python program to find the greatest number.py
+++ python program to find the greatest number.py
@@ -1,7 +1,7 @@
-a=int(input("enter the 1st number:"))
+a=int(input("enter the 1st number:"))#here we are getting the input from the user 
 b=int(input("enter the 2nd number:"))
 c=int(input("enter the 3rd number:"))
-if a>b and a>c:
+if a>b and a>c: #we are using comparitive operation and logical operation to find the greatest
     print(" 1st is great")
 elif b>a and b>c:
     print("2nd is great")
