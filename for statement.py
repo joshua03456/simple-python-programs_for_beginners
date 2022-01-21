99348997f85788d38e132ac99181875cd36f583a
joshua03456 <80635235+joshua03456@users.noreply.github.com>
2022-01-21 16:08:24 +0530

    Add files via upload

---
+++ for statement.py
@@ -0,0 +1,7 @@
+"""
+this is a program to display even numbers from 1 to 50
+"""
+for i in range(50):#here we are using range function to display the numbers
+    if i%2!=1 and i%5!=0:#here we are giving the condition for sorting the even numbers from 1 to 50
+        print(i)
+        
